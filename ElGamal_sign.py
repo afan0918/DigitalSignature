c883bb5d5829bfa720b25c11ff25a15e000f3ae8
afan <afan890918+github@gmail.com>
2023-09-25 18:26:57 +0800

    add ElGamal sign algorithm

---
+++ ElGamal_sign.py
@@ -0,0 +1,49 @@
+import hashlib
+import math
+import random
+
+from Crypto.Util.number import getPrime, bytes_to_long, long_to_bytes
+
+# by https://en.wikipedia.org/wiki/ElGamal_signature_scheme
+
+# 1. Key generation
+# 1-1. Parameter generation
+p = getPrime(1024)
+q = getPrime(1024)
+H = 'sha256'
+g = random.randint(1, p - 1)
+
+# 1-2. Per-user keys
+x = random.randint(1, p - 2)
+y = pow(g, x, p)
+
+m = b'afan'
+
+
+# 2. Signing
+def sign(m, p, g, x, H):
+    while True:
+        k = random.randint(2, p - 2)
+        if math.gcd(k, p - 1) != 1:
+            continue
+        r = pow(g, k, p)
+        h = hashlib.new(H)
+        h.update(m)
+        hm = int(h.hexdigest(), 16)
+        s = (hm - x * r) * pow(k, -1, p - 1) % (p - 1)
+        if s != 0:
+            return r, s
+
+
+# 3. Verifying
+def verify(m, r, s, H):
+    if (r <= 0) | (r >= p) | (s <= 0) | (s >= p - 1):
+        return False
+    h = hashlib.new(H)
+    h.update(m)
+    hm = int(h.hexdigest(), 16)
+    return pow(g, hm, p) == (pow(y, r, p) * pow(r, s, p)) % p
+
+
+r, s = sign(m, p, g, x, H)  # 簽名
+print(verify(m, r, s, H))  # 用私鑰驗證
--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 
 目前實現演算法
 
-### RSA and Rabin Signature
-* RSA sign
-* RSA hash sign
-* Rabin sign
+### RSA & Rabin Signature （第三章）
+* [RSA sign](https://github.com/afan0918/DigitalSignature/blob/master/rsa_sign.py)
+* [RSA hash sign](https://github.com/afan0918/DigitalSignature/blob/master/rsa_hash_sign.py)
+* [Rabin sign](https://github.com/afan0918/DigitalSignature/blob/master/Rabin_sign.py)
 
-### One-Time Signature
-* Lamport sign
-* 
+### One-Time Signature （第四章）
+* [Lamport sign](https://github.com/afan0918/DigitalSignature/blob/master/Lamport_sign.py)
+
+### ElGamal Signature & Schnorr Signature （第五章）
+* [ElGamal Signature](https://github.com/afan0918/DigitalSignature/blob/master/ElGamal_sign.py)
+
